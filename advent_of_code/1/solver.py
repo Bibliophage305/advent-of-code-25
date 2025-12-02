0b00b960cb063599edf773461ec4c50ee77c4964
Chris Hampson <christopher.hampson58@gmail.com>
2025-12-02 10:40:18 +0000

    Improve day 1

--- advent_of_code/1/solver.py
+++ advent_of_code/1/solver.py
@@ -1,5 +1,5 @@
 from advent_of_code import advent
-from itertools import chain
+from itertools import chain, accumulate
 
 
 class Solver(advent.Advent):
@@ -10,15 +10,10 @@ class Solver(advent.Advent):
         return [[int(x.strip()[1:]) if x.strip()[0] == "R" else -int(x.strip()[1:]) for x in data]]
     
     def zero_count(self, steps):
-        position, zero_count = 50, 0
-        for step in steps:
-            position += step
-            if position % 100 == 0:
-                zero_count += 1
-        return zero_count
+        return sum(x % 100 == 0 for x in accumulate(steps, initial=50))
 
     def part_1(self, steps):
         return self.zero_count(steps)
 
     def part_2(self, steps):
-        return self.zero_count(chain.from_iterable((step // abs(step) for _ in range(abs(step))) for step in steps))
+        return self.zero_count(chain.from_iterable([step // abs(step)] * abs(step) for step in steps))
